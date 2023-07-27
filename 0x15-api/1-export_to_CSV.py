e22a5a088f83ed2db1d083b84076a1e560a57e15
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-07-27 02:49:06 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,17 +5,19 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    base_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    user_info = requests.get(base_url).json()
+    usr_id = int(sys.argv[1])
+    b_url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(usr_id)
+    user_info = requests.get(url).json()
     username = user_info.get("username")
-    tasks_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
-    tasks = requests.get(tasks_url).json()
+    t_url = b_url + "todos?userId={}".format(usr_id)
+    tasks = requests.get(t_url).json()
     completed_tasks = [task for task in tasks if task.get("completed")]
 
     # Generating CSV file
-    csv_file = "{}.csv".format(employee_id)
+    csv_file = "{}.csv".format(usr_id)
     with open(csv_file, mode="w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in completed_tasks:
-            writer.writerow([employee_id, username, task["completed"], task["title"]])
+            writer.writerow(
+                    [usr_id, username, task["completed"], task["title"]])
