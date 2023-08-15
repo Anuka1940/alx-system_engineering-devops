f891dbfac4de19e9bd0a4b5d2ce393b8e7365ff2
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-15 04:07:18 -0700

    more on task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-""" Module for task 1"""
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts
-    listed for a given subreddit"""
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
     import requests
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "My Reddit API Client"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-
-        if not posts:
-            print("None")
-            return
-        for post in posts[:10]:
-            title = post.get("data", {}).get("title")
-            print(title)
-        else:
-            print("Invalid subreddit.")
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
