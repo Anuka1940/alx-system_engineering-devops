ea1a8f6399bb012bb0cdc21b57e4cb2fb210b9b5
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-15 03:46:23 -0700

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module for task 1"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "My Reddit API Client"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+
+        if not posts:
+            print("None")
+            return
+        for post in posts[:10]:
+            title = post.get("data", {}).get("title")
+            print(title)
+        else:
+            print("Invalid subreddit.")
