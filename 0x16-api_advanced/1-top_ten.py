a43ccb293aa1cdb7293a99c32472ccda0a0906fe
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-15 03:55:59 -0700

    fix error

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     listed for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "My Reddit API Client"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
