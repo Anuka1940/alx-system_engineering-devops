b47d1324fad98896c7b4554867d95fb7ce5139cd
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-15 23:39:03 -0700

    fix error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -17,8 +17,8 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
 
     hot_1 = hot_list + [child.get("data").get("title")
                         for child in response.json()
-                        .get("data", {})
-                        .get("children", {})]
+                        .get("data")
+                        .get("children")]
 
     info = response.json()
     if not info.get("data").get("after"):
