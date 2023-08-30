b1159ed976dc1e91ece96df3afa54ea315099170
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-30 04:58:00 -0700

    client config file with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Puppet scrip to config ssh file
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line  => '      PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+}
