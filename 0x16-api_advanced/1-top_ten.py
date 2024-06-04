933bb0f88dc9c36990f49c3e4989456d8341047a
Abdul Lateef Anuka <anukalateef@gmail.com>
2024-06-04 21:01:20 +0100

    Upload rewrite tast 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""This module uses the reddit api to fetch data"""
+
+import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
+    """Fetch top 10 posts using reddit api"""
+    posts = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
+    if posts.status_code >= 300:
+        print("None")
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        children = posts.json().get('data').get('children')
+        [print(child.get('data').get('title'))
+            for child in children]
